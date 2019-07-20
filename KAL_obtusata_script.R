96b28c382e66737b8d87460a6efdeefee2ce3c3a
adrianachumacero <abc157@scarletmail.rutgers.edu>
2019-07-20 18:05:05 +0800

    Recalculated GSI using total/gonad * 100

--- KAL_obtusata_script.R
+++ KAL_obtusata_script.R
@@ -36,23 +36,14 @@ Obtusata_KAL_LW_plot
 Obtusata_KAL_GSI <- subset(Obtusata_KAL, !is.na(Gonad_Weight))
 
 #GSI
-Obtusata_KAL_GSI$GSI <- (Obtusata_KAL_GSI$Gonad_Weight/(Obtusata_KAL_GSI$Weight - Obtusata_KAL_GSI$Gonad_Weight)) * 100
+Obtusata_KAL_GSI$GSI <- (Obtusata_KAL_GSI$Gonad_Weight/Obtusata_KAL_GSI$Weight) * 100
 ggplot(data = Obtusata_KAL_GSI, aes(x = Standard_Length, y = GSI)) + 
   geom_point()
 
-Obtusata_KAL_GSI$GSI_2 <- (Obtusata_KAL_GSI$Gonad_Weight/Obtusata_KAL_GSI$Weight) * 100
-ggplot(data = Obtusata_KAL_GSI, aes(x = Standard_Length, y = GSI_2)) + 
-  geom_point()
-
 mean(Obtusata_KAL_GSI[["GSI"]])
-mean(Obtusata_KAL_GSI[["GSI_2"]])
 
 Obtusata_KAL_GSI$logGSI <- log(Obtusata_KAL_GSI$GSI)
 ggplot(data = Obtusata_KAL_GSI, aes(x = Standard_Length, y = logGSI)) +
-  geom_point()
-
-Obtusata_KAL_GSI$logGSI_2 <- log(Obtusata_KAL_GSI$GSI_2)
-ggplot(data = Obtusata_KAL_GSI, aes(x = Standard_Length, y = logGSI_2)) +
   geom_point()
 
 #####Compare relationship between logGSI and total length/weight#####
@@ -61,21 +52,14 @@ ggplot(data = Obtusata_KAL_GSI, aes(x = Standard_Length, y = logGSI_2)) +
 ggplot(data = Obtusata_KAL_GSI, aes(x = Total_Length, y = logGSI)) +
   geom_point()
 
-ggplot(data = Obtusata_KAL_GSI, aes(x = Total_Length, y = logGSI_2)) +
-  geom_point()
-
 #logGSI and Weight
 ggplot(data = Obtusata_KAL_GSI, aes(x = Weight, y = logGSI)) +
-  geom_point()
-
-ggplot(data = Obtusata_KAL_GSI, aes(x = Weight, y = logGSI_2)) +
   geom_point()
 
 #####GLM comparison#####
 
 #checking distribution of GSI data
 KAL_GSI_dist <- hist(Obtusata_KAL_GSI$GSI)
-KAL_GSI_dist <- hist(Obtusata_KAL_GSI$GSI_2)
 
 #log transform TL, SL, and Weight
 Obtusata_KAL_GSI$logTL <- log(Obtusata_KAL_GSI$Total_Length)
@@ -87,15 +71,7 @@ GSI_KAL_SL_glm <- glm(Obtusata_KAL_GSI$GSI~Obtusata_KAL_GSI$Standard_Length, fam
 GSI_KAL_TL_glm <- glm(Obtusata_KAL_GSI$GSI~Obtusata_KAL_GSI$Total_Length, family = Gamma)
 GSI_KAL_Weight_glm <- glm(Obtusata_KAL_GSI$GSI~Obtusata_KAL_GSI$Weight, family = Gamma)
 
-GSI2_KAL_SL_glm <- glm(Obtusata_KAL_GSI$GSI_2~Obtusata_KAL_GSI$Standard_Length, family = Gamma)
-GSI2_KAL_TL_glm <- glm(Obtusata_KAL_GSI$GSI_2~Obtusata_KAL_GSI$Total_Length, family = Gamma)
-GSI2_KAL_Weight_glm <- glm(Obtusata_KAL_GSI$GSI_2~Obtusata_KAL_GSI$Weight, family = Gamma)
-
 #check significance of each model
 AIC(GSI_KAL_SL_glm)
 AIC(GSI_KAL_TL_glm) #most significant model
 AIC(GSI_KAL_Weight_glm)
-
-AIC(GSI2_KAL_SL_glm)
-AIC(GSI2_KAL_TL_glm) #still most significant model
-AIC(GSI2_KAL_Weight_glm)

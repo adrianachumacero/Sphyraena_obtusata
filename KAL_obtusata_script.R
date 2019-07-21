cbc912273d06b544098414717d201730b01136d5
adrianachumacero <abc157@scarletmail.rutgers.edu>
2019-07-21 21:19:51 +0800

    small changes to KAL histogram

--- KAL_obtusata_script.R
+++ KAL_obtusata_script.R
@@ -24,11 +24,17 @@ summary(KAL_lm_lLlW)
 
 #plot model
 Obtusata_KAL_logLW_plot <- ggplot(data = Obtusata_KAL_LWR, aes(x = logL, y = logW)) +
-  geom_point() + geom_smooth(method = "lm")
+  geom_point() + geom_smooth(method = "lm") + ggtitle(label = "logLWR of S.obtusata in Kalibo")
 Obtusata_KAL_LW_plot <- ggplot(data = Obtusata_KAL_LWR, aes(x = Total_Length, y = Weight)) +
-  geom_point()
+  geom_point() + ggtitle(label = "LWR of S.obtusata in Kalibo")
 Obtusata_KAL_logLW_plot
 Obtusata_KAL_LW_plot
+
+#another plot but with axes names
+plot(Obtusata_KAL_LWR$logW~Obtusata_KAL_LWR$logL, pch=19, col="black", xlab="logLength (cm)", ylab= "logWeight (g)",
+     main = "logLWR of S.obtusata in Kalibo")
+plot(Obtusata_KAL_LWR$Weight~Obtusata_KAL_LWR$Total_Length, pch=19, col="black", xlab="Length (cm)", ylab= "Weight (g)", 
+     main = "LWR of S.obtusata in Kalibo")
 
 #####Calculating GSI#####
 
@@ -37,34 +43,32 @@ Obtusata_KAL_GSI <- subset(Obtusata_KAL, !is.na(Gonad_Weight))
 
 #GSI
 Obtusata_KAL_GSI$GSI <- (Obtusata_KAL_GSI$Gonad_Weight/Obtusata_KAL_GSI$Weight) * 100
-ggplot(data = Obtusata_KAL_GSI, aes(x = Standard_Length, y = GSI)) + 
-  geom_point()
+##ggplot(data = Obtusata_KAL_GSI, aes(x = Standard_Length, y = GSI)) + geom_point()
 
 mean(Obtusata_KAL_GSI[["GSI"]])
 
 Obtusata_KAL_GSI$logGSI <- log(Obtusata_KAL_GSI$GSI)
-ggplot(data = Obtusata_KAL_GSI, aes(x = Standard_Length, y = logGSI)) +
-  geom_point()
+##ggplot(data = Obtusata_KAL_GSI, aes(x = Standard_Length, y = logGSI)) + geom_point()
 
 #####Compare relationship between logGSI and total length/weight#####
 
-#logGSI and TL
-ggplot(data = Obtusata_KAL_GSI, aes(x = Total_Length, y = logGSI)) +
-  geom_point()
+#raw GSI and TL
+ggplot(data = Obtusata_KAL_GSI, aes(x = Total_Length, y = GSI)) + 
+  geom_point() + ggtitle(label = "Relationship between GSI and total length", subtitle = "S.obtusata in Kalibo")
 
 #logGSI and Weight
-ggplot(data = Obtusata_KAL_GSI, aes(x = Weight, y = logGSI)) +
-  geom_point()
+##ggplot(data = Obtusata_KAL_GSI, aes(x = Weight, y = logGSI)) + geom_point()
 
 #####GLM comparison#####
 
 #checking distribution of GSI data
 KAL_GSI_dist <- hist(Obtusata_KAL_GSI$GSI)
+plot(KAL_GSI_dist, xlab = "GSI", ylab = "Frequency", main = "Distribution of GSI for Kalibo")
 
 #log transform TL, SL, and Weight
-Obtusata_KAL_GSI$logTL <- log(Obtusata_KAL_GSI$Total_Length)
-Obtusata_KAL_GSI$logSL <- log(Obtusata_KAL_GSI$Standard_Length)
-Obtusata_KAL_GSI$logWeight <- log(Obtusata_KAL_GSI$Weight)
+##Obtusata_KAL_GSI$logTL <- log(Obtusata_KAL_GSI$Total_Length)
+##Obtusata_KAL_GSI$logSL <- log(Obtusata_KAL_GSI$Standard_Length)
+##Obtusata_KAL_GSI$logWeight <- log(Obtusata_KAL_GSI$Weight)
 
 #making GLMs
 GSI_KAL_SL_glm <- glm(Obtusata_KAL_GSI$GSI~Obtusata_KAL_GSI$Standard_Length, family = Gamma)
